e2ea8946ce66f67d8f01fa4e372bd7d0f71bb65f
Houssem <ousjihoussem01@gmail.com>
2022-02-19 13:35:37 +0100

    the js files

--- js/script.js
+++ js/script.js
@@ -12,3 +12,36 @@ document.onclick = (e) => {
         nav_menu.classList.remove("active");
     }
 }
+
+var tab1 = document.getElementById("ftab");
+var tab2 = document.getElementById("stab");
+var tab3 = document.getElementById("ltab");
+var description = document.getElementsByClassName("desc_item");
+tab1.onclick = () =>{
+    console.log(description[0].style.display = "block");
+    console.log(description[1].style.display = "none");
+    console.log(description[2].style.display = "none");
+}
+tab2.onclick = () =>{
+    console.log(description[0].style.display = "none");
+    console.log(description[1].style.display = "block");
+    console.log(description[2].style.display = "none");
+}
+tab3.onclick = () =>{
+    console.log(description[0].style.display = "none");
+    console.log(description[1].style.display = "none");
+    console.log(description[2].style.display = "block");
+}
+/*
+tab2.onclick = () =>{
+    console.log(description[0])
+    console.log(description[])
+    console.log(description[1])
+    console.log(description[2].style.display = "block");
+    console.log(description[3].style.display = "none");
+}
+tab3.onclick = () =>{
+    console.log(description[1].style.display = "none");
+    console.log(description[2].style.display = "none");
+    console.log(description[3].style.display = "block");
+}*/
